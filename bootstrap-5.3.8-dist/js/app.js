f153d2bd6e287b871e581ec7f23d2b5b6acfb75c
AcsBeni <98952007+AcsBeni@users.noreply.github.com>
2025-09-08 09:27:34 +0200

    Add files via upload

---
+++ bootstrap-5.3.8-dist/js/app.js
@@ -0,0 +1,40 @@
+let lightmodeBtn = document.querySelector("#lightmodeBtn")
+let darkmodeBtn = document.querySelector("#darkmodeBtn")
+
+let theme = 'light';
+
+lightmodeBtn.addEventListener("click",() =>{
+    setTheme("light");
+    saveTheme("light");
+});
+darkmodeBtn.addEventListener("click",()=>{
+    setTheme("dark");
+    saveTheme("dark");
+});
+function loadTheme(){
+    theme = "light"
+    if (localStorage.getItem("SCTheme")) {
+        theme = localStorage.getItem("SCTheme");
+    }
+    setTheme(theme)
+}
+
+function saveTheme(theme){
+    localStorage.setItem('SCTheme', theme)
+}
+function setTheme(theme){
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    setThemeBtn(theme)
+}
+function setThemeBtn(theme){
+    if (theme=='light') {
+        lightmodeBtn.classList.add("hide");
+        darkmodeBtn.classList.remove("hide");
+    }
+    else{
+        lightmodeBtn.classList.remove("hide");
+        darkmodeBtn.classList.add("hide");
+    }
+}
+loadTheme();
+
